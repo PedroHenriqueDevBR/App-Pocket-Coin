eeac48d0987899584b76bd2656ade0dd55ab8cba
PedroHenriqueBR2 <p.lyma18@gmail.com>
2019-01-19 12:27:22 -0300

    Correção de bugs na MainActivity.

--- app/src/main/java/com/droppages/pedrohenrique/pocketcoin/MainActivity.java
+++ app/src/main/java/com/droppages/pedrohenrique/pocketcoin/MainActivity.java
@@ -18,32 +18,32 @@ import com.droppages.pedrohenrique.pocketcoin.view.CarteiraActivity;
 import com.droppages.pedrohenrique.pocketcoin.view.CategoriaActivity;
 import com.droppages.pedrohenrique.pocketcoin.view.TagActivity;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private Toolbar                 toolbar;
+    private FloatingActionButton    fab;
+    private DrawerLayout            drawer;
+    private ActionBarDrawerToggle   toggle;
+    private NavigationView          navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        // Bind
+        toolbar = findViewById(R.id.toolbar);
+        drawer  = findViewById(R.id.drawer_layout);
+        fab     = findViewById(R.id.fab);
+
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        fab.setOnClickListener(
+                c -> Snackbar.make(this.navigationView, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+        );
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
 
@@ -55,28 +55,6 @@ public class MainActivity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -99,7 +77,7 @@ public class MainActivity extends AppCompatActivity
             cadastrarTag();
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
@@ -110,17 +88,11 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-    public void cadastrarCategoria(){
-        startActivity(new Intent(this, CategoriaActivity.class));
-    }
+    public void cadastrarCategoria(){ startActivity(new Intent(this, CategoriaActivity.class)); }
 
 
-    public void cadastrarTag(){
-        startActivity(new Intent(this, TagActivity.class));
-    }
+    public void cadastrarTag(){ startActivity(new Intent(this, TagActivity.class)); }
 
 
-    public void sair(){
-        this.finish();
-    }
+    public void sair(){ this.finish(); }
 }
