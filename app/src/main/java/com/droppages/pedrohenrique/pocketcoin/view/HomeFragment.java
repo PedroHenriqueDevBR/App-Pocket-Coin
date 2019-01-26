fc2b5a21510a2b86c6945d332fc60bc92e37bab9
PedroHenriqueBR2 <p.lyma18@gmail.com>
2019-01-26 12:29:49 -0300

    Criação de funções para listar o relatório mensal, setarTodosOsDados(), setarSaldoTotal(), setarVisaoGeral(), setarBalancoMensal(), setarDespesasPorCategoria(), calcularEconomiaMesal()

--- app/src/main/java/com/droppages/pedrohenrique/pocketcoin/view/HomeFragment.java
+++ app/src/main/java/com/droppages/pedrohenrique/pocketcoin/view/HomeFragment.java
@@ -3,29 +3,35 @@ package com.droppages.pedrohenrique.pocketcoin.view;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.se.omapi.Session;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.droppages.pedrohenrique.pocketcoin.R;
+import com.droppages.pedrohenrique.pocketcoin.adapters.DespesaPorCategoriaAdapter;
+import com.droppages.pedrohenrique.pocketcoin.controllers.CategoriaController;
 import com.droppages.pedrohenrique.pocketcoin.controllers.MovimentacaoController;
 import com.droppages.pedrohenrique.pocketcoin.dal.App;
 import com.droppages.pedrohenrique.pocketcoin.dal.Sessao;
-import com.droppages.pedrohenrique.pocketcoin.model.Movimentacao;
+import com.droppages.pedrohenrique.pocketcoin.model.Categoria;
+import com.droppages.pedrohenrique.pocketcoin.model.NaturezaDaAcao;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import io.objectbox.Box;
 import io.objectbox.BoxStore;
 
 
 public class HomeFragment extends Fragment {
-    private BoxStore boxStore;
-    private MovimentacaoController controller;
-    private TextView txtSaldoAtual;
+    private BoxStore                boxStore;
+    private MovimentacaoController  movimentacaoController;
+    private CategoriaController     categoriaController;
+    private TextView                txtSaldoAtual, txtReceitaGeral, txtDespesaGeral, txtReceitaMensal, txtDespesaMensal, txtEconomia;
+    private RecyclerView            recyclerView;
 
     public HomeFragment() {}
 
@@ -35,28 +41,90 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         // Bind
-        txtSaldoAtual = view.findViewById(R.id.text_view_saldo_atual);
-        boxStore = ((App)getActivity().getApplication()).getBoxStore();
-        controller = new MovimentacaoController(boxStore, getActivity().getSharedPreferences(Sessao.SESSAO_USUARIO, Context.MODE_PRIVATE));
+        txtSaldoAtual           = view.findViewById(R.id.text_view_saldo_atual);
+        txtReceitaGeral         = view.findViewById(R.id.text_view_receita_geral);
+        txtDespesaGeral         = view.findViewById(R.id.text_view_despesa_geral);
+        txtReceitaMensal        = view.findViewById(R.id.text_view_receita_mensal);
+        txtDespesaMensal        = view.findViewById(R.id.text_view_despesa_mensal);
+        txtEconomia             = view.findViewById(R.id.text_view_economia);
+        recyclerView            = view.findViewById(R.id.lista_gasto_por_categoria);
+        boxStore                = ((App)getActivity().getApplication()).getBoxStore();
+        movimentacaoController  = new MovimentacaoController(boxStore, getActivity().getSharedPreferences(Sessao.SESSAO_USUARIO, Context.MODE_PRIVATE));
+        categoriaController     = new CategoriaController(boxStore, getActivity().getSharedPreferences(Sessao.SESSAO_USUARIO, Context.MODE_PRIVATE));
 
-        setarSaldoTotal();
+        // Metodos
+        setarTodosOsDados();
 
         return view;
     }
 
     @Override
     public void onResume() {
-        setarSaldoTotal();
+        setarTodosOsDados();
         super.onResume();
     }
 
-    private void setarSaldoTotal(){
-        String valor = Float.toString(controller.calcularSaldoTotalDeTodasAsCarteiras());
+    private void setarTodosOsDados() {
+        setarSaldoTotal();
+        setarVisaoGeral();
+        setarBalancoMensal();
+        setarDespesasPorCategoria();
+        calcularEconomiaMesal();
+    }
+
+    private void setarSaldoTotal() {
+        String valor = Float.toString(movimentacaoController.calcularSaldoTotalDeTodasAsCarteiras());
         txtSaldoAtual.setText(valor);
     }
 
-    private void mostrarListaDeMovimentacoes(){
-        List<Movimentacao> movimentacoes = controller.selecionarTodasMovimentacoesDoUsuario();
+    private void setarVisaoGeral() {
+        String receita = Float.toString(movimentacaoController.calcularMovimentacaoTotalDoMesEAnoAtualAPartirDaNatureza("Crédito"));
+        String despesa = Float.toString(movimentacaoController.calcularMovimentacaoTotalDoMesEAnoAtualAPartirDaNatureza("Débito"));
+        txtReceitaGeral.setText("R$ " + receita);
+        txtDespesaGeral.setText("R$ " + despesa);
+    }
+
+    private void setarBalancoMensal() {
+        String receita = Float.toString(movimentacaoController.calcularMovimentacaoTotalDoMesEAnoAtualAPartirDaNatureza("Crédito"));
+        String despesa = Float.toString(movimentacaoController.calcularMovimentacaoTotalDoMesEAnoAtualAPartirDaNatureza("Débito"));
+        txtReceitaMensal.setText("R$ " + receita);
+        txtDespesaMensal.setText("R$ " + despesa);
+    }
+
+    private void setarDespesasPorCategoria() {
+        List<Categoria> categorias = categoriaController.selecionarTodasAsCategoriasDespesaDoUsuarioLogado();
+        List<DespesaPorCategoriaAdapter.CategoriaComValor> listaCategoriaComValor = new ArrayList<>();
+
+        for (Categoria categoria : categorias){
+            DespesaPorCategoriaAdapter.CategoriaComValor categoriaComValor = new DespesaPorCategoriaAdapter.CategoriaComValor();
+            categoriaComValor.categoria = categoria.getNome();
+            categoriaComValor.valor = (float) movimentacaoController.calcularValorGastoNoMesPorCategoriaSelecionada(categoria);
+            listaCategoriaComValor.add(categoriaComValor);
+        }
+
+        DespesaPorCategoriaAdapter adapter = new DespesaPorCategoriaAdapter(listaCategoriaComValor, getActivity().getApplicationContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    private void calcularEconomiaMesal(){
+        float despesa   = movimentacaoController.calcularMovimentacaoTotalDoMesEAnoAtualAPartirDaNatureza("Débito");
+        float receita   = movimentacaoController.calcularMovimentacaoTotalDoMesEAnoAtualAPartirDaNatureza("Crédito");
+        int difereca    = (int) (receita - despesa);
+        int resultado   = (int) ((despesa * 100) / receita);
+        int economia    = 100 - resultado;
+
+        String msg;
+        if (economia == 0) {
+            msg = "Neste mês sua economia foi zero. Seu gasto mensal é equivalente a entrada de dinheiro.";
+        } else if (economia > 0) {
+            msg = "Parabéns, sua economia no mês atual é "+ economia +"%, sua economia líquida é igual a R$ "+ difereca;
+        } else {
+            economia *= -1;
+            difereca *= -1;
+            msg = "No mês atual a sua despesa superou a receita, o seu gasto superou a receita em " + economia + "% cuidado para não perder o controle das suas contas";
+        }
+        txtEconomia.setText(msg);
     }
 
 }
